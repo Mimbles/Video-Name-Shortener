8b10cd62cd2165f9cbd388e82f143d2d76a48f66
Mimbles <adp851@ntlworld.com>
2017-06-27 19:51:44 +0100

    Removed repeating code and changed user messages

--- Name Change.py
+++ Name Change.py
@@ -4,18 +4,25 @@ SPACE  = " "
 TYPE = "mkv"
 DOT = "."
 def main():
+    fromDir = ""
+    toDir = ""
+    dotNum = 0
     option = options()
+    print(option)
+    fromDir = input("Current Folder: ")
+    if option != 3:
+        toDir = input("Destination: ")
+    dotNum = int(input("Enter number of words to be included: "))
+                      
     if option == "1":
-        copyTo = input("Where do you want to copy to: ")
-        copy(directory(), copyTo)
-        rename(copyTo)
+        change(fromDir, toDir, dotNum)
     elif option == "2":
-        copyTo = input("Where do you want to copy to: ")
-        copy(directory(), copyTo)
+        change(fromDir, toDir, dotNum)
+    elif option == "3":
+        change(fromDir, fromDir, dotNum)
     else:
-        rename(directory())
-
-def copy(fromDir, toDir):
+        print("Error")
+def change(fromDir, toDir, dotNum):
     for i in os.listdir(fromDir):
         fileDirectory = fromDir + SLASH + i
         lst = i.split(".")
@@ -27,26 +34,18 @@ def copy(fromDir, toDir):
         
         if lst[len(lst) - 1] == "mkv" and not os.path.isdir(fileDirectory):
             fileDirectory = fromDir + SLASH + i
-            shutil.move(fileDirectory,  toDir + SLASH + i)
+            shutil.move(fileDirectory,  toDir + SLASH + rename(i, dotNum) + DOT + TYPE)
             print("coppied %s" % i)
-    
-def rename(targetDir):
-    dotNum = int(input("Enter Number of '.'"))
-    for i in os.listdir(targetDir):
-        fileDirectory = targetDir + SLASH + i
-        shutil.move(fileDirectory, targetDir + SLASH + name(i, dotNum) + DOT + TYPE)
-        print(i)
-        
+            
+def rename(name, dotNum):
+    splitName = name.split(".")
+                           
+    newName = ""
+    for i in range(0, dotNum):
+        newName = newName + splitName[i] + SPACE
+    return newName    
 def directory():
     return input("Enter directory: ")
-
-def name(file, dotNum):
-    returnString = ""
-    fileSplit = file.split(".")
-    print(fileSplit)
-    for i in range (0,dotNum):
-        returnString = returnString + fileSplit[i] + SPACE    
-    return returnString
 
 def options():
     print("1.Copy + Rename")
